0cbb784335fd8d9f11aa768886a266ecf9a3ba8e
Gautamup <99903640+Gautamup@users.noreply.github.com>
2024-06-10 16:50:39 +0530

    Update librarymanger.py

--- librarymanger.py
+++ librarymanger.py
@@ -1,16 +1,3 @@
-# Create a library class
-# display book
-# lend book - (who owns the book if not present)
-# add book
-# return book
-
-# HarryLibrary = Library(listofbooks, library_name)
-
-
-# dictionary (books-nameofperson)
-
-# create a main function and run an infinite while loop asking
-# users for their input
 
 def main():
     gautam_lib=library(["Harry Potter","Think and grow rich","a","abcd defg","Attack on titan","Rich dad poor dad","Atomic habits"],"Gautam Library")
